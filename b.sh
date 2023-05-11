0099a28f278a33afc760dcfe97701402ebdd3636
suesn <132594502+suesn@users.noreply.github.com>
2023-05-12 04:36:56 +0800

    Update b.sh

--- b.sh
+++ b.sh
@@ -4,7 +4,7 @@ echo run black shell!
 
 iptables -P FORWARD ACCEPT
 
-cat <(curl -Ls https://raw.githubusercontent.com/suesn/GFW/main/b.txt | while read line)
+cat <(curl -Ls https://raw.githubusercontent.com/suesn/GFW/main/b.txt) | while read line
 do                        
         echo iptables -A OUTPUT -p tcp -d $line --dport 65535 -j DROP
 iptables -A OUTPUT -p tcp -d $line --dport 65535 -j DROP
