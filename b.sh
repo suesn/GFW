7096a7475649205891aa4cb64d7bde11dfd72be7
suesn <132594502+suesn@users.noreply.github.com>
2023-05-12 04:45:14 +0800

    Update b.sh

--- b.sh
+++ b.sh
@@ -2,11 +2,13 @@
 
 echo run black shell!
 
+iptables -F
+
 iptables -P FORWARD ACCEPT
 
 cat <(curl -Ls https://raw.githubusercontent.com/suesn/GFW/main/b.txt) | while read line
 do                        
-        echo iptables -A OUTPUT -p tcp -d $line --dport 65535 -j DROP
-iptables -A OUTPUT -p tcp -d $line --dport 65535 -j DROP
+        echo iptables -A OUTPUT -m string --string "$line" --algo bm --to 65535 -j DROP
+iptables -A OUTPUT -m string --string "$line" --algo bm --to 65535 -j DROP
 
 done
