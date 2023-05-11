5dc9d907bf9e5c9952f2ee0d40aadf5c0b9d288e
suesn <132594502+suesn@users.noreply.github.com>
2023-05-12 04:31:50 +0800

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+echo run black shell!
+
+iptables -P FORWARD ACCEPT
+
+cat https://raw.githubusercontent.com/suesn/GFW/main/b.txt | while read line
+do                        
+        echo iptables -A OUTPUT -p tcp -d $line --dport 65535 -j DROP
+iptables -A OUTPUT -p tcp -d $line --dport 65535 -j DROP
+
+done
