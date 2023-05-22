9315461bdbc28b71171cb38fad8efde6f01582a6
suesn <132594502+suesn@users.noreply.github.com>
2023-05-23 00:51:30 +0800

    Update b.sh

--- b.sh
+++ b.sh
@@ -8,9 +8,9 @@ iptables -P FORWARD ACCEPT
 
 cat <(curl -Ls https://raw.githubusercontent.com/suesn/GFW/main/b.txt) | while read line
 do                        
-        echo iptables -A OUTPUT -m string --string  $line  --algo bm --to 65535 -j DROP
-iptables -A OUTPUT -m string --string  $line  --algo bm --to 65535 -j DROP
-        echo iptables -A INPUT -m string --string  $line  --algo bm --to 65535 -j DROP
-iptables -A INPUT -m string --string  $line  --algo bm --to 65535 -j DROP
+        echo iptables -A OUTPUT -m string --string "$line" --algo bm --to 65535 -j DROP
+iptables -A OUTPUT -m string --string "$line" --algo bm --to 65535 -j DROP
+        echo iptables -A INPUT -m string --string "$line" --algo bm --to 65535 -j DROP
+iptables -A INPUT -m string --string "$line" --algo bm --to 65535 -j DROP
 
 done
